a9d59f464eae66263a382858e8fff526aa6f9831
Pawcio1w <plewanczyk95@wp.pl>
2022-11-23 19:27:25 +0100

    Dodanie petli ForEach

---
+++ src/ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] cats=new String[]{"Burek", "Mruczuś", "Garfield", "Iskierka"};
+        for (String cat:cats) {
+            System.out.println(cat);
+        }
+    }
+}
